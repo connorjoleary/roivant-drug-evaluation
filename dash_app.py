21e90bd64b6b14b26c91d05c33d19479875a56fd
connorjoleary <oleary.connor24@gmail.com>
2020-09-28 18:17:46 -0600

    inital dashboard setup

--- dash_app.py
+++ dash_app.py
@@ -1,7 +1,13 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+import dash_table
+
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
 import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -13,38 +19,79 @@ colors = {
     'text': '#7FDBFF'
 }
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+df = pd.read_csv('drug_data.csv')
 
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
+fig = make_subplots(rows=1, cols=2,
+    subplot_titles=("Price", "Time to Develop"))
+
+fig.add_trace(
+    px.box(df, y="price_annual", hover_data=['name'], points="all", title='Price').data[0],
+    row=1, col=1
 )
+
+fig.add_trace(
+    px.box(df, y="time_to_dev_weeks", hover_data=['name'], points="all").data[0],
+    row=1, col=2
+)
+
+fig.update_layout(height=700)
+
+@app.callback(
+    Output('datatable-row-ids-container', "figure"),
+    [Input('datatable-interactivity', "derived_virtual_data"),
+     Input('datatable-interactivity', "derived_virtual_selected_rows")])
+     
+    if derived_virtual_selected_rows is None:
+        derived_virtual_selected_rows = []
+
+    dff = df if rows is None else pd.DataFrame(rows)
+
+    colors = ['#7FDBFF' if i in derived_virtual_selected_rows else '#0074D9'
+              for i in range(len(dff))]
+
+    return px.box(df, y="price_annual", hover_name='name', points="all", title='Price', color=colors)
+
+# fig.update_layout(
+#     plot_bgcolor=colors['background'],
+#     paper_bgcolor=colors['background'],
+#     font_color=colors['text']
+# )
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     html.H1(
-        children='Hello Dash',
+        children='Roivant Drug Information',
         style={
             'textAlign': 'center',
             'color': colors['text']
         }
     ),
 
-    html.Div(children='Dash: A web application framework for Python.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
+    dash_table.DataTable(
+        id='datatable-interactivity',
+        columns=[
+            {"name": i, "id": i, "deletable": False, "selectable": False} for i in df.columns
+        ],
+        data=df.to_dict('records'),
+        editable=False,
+        filter_action="native",
+        sort_action="native",
+        sort_mode="multi",
+        row_selectable="multi",
+        row_deletable=False,
+        selected_columns=[],
+        selected_rows=[],
+        page_action="native",
+        page_current= 0,
+        page_size= 10,
+        style_table={
+            'overflowY': 'scroll'
+        }
+    ),
+    dcc.Graph(id='datatable-row-ids-container'),
 
     dcc.Graph(
-        id='example-graph-2',
+        id='example-graph-1',
         figure=fig
     )
 ])
--- produce_dummy_data.py
+++ produce_dummy_data.py
@@ -1,16 +1,27 @@
 import pandas as pd
 import numpy as np
+import random
+import string
+
+random.seed(1)
+np.random.seed(1)
 
 def random_number(num, mean, sts=None):
     if not sts:
         sts = mean*.1
-    np.random.seed(1)
     return np.random.normal(mean, sts, num).astype(int)
+
+def random_string(choices=string.ascii_lowercase, k=13):
+    return ''.join(random.choices(string.ascii_lowercase, k=k))
 
 def produce_dummy_data(num=50):
     df = pd.DataFrame()
     df['price_annual'] = random_number(num, 450)
     df['time_to_dev_weeks'] = random_number(num, 52)
+
+
+    df['name'] = df.price_annual.apply(random_string)
+    df.set_index('name', inplace=True)
     print(df.head())
 
     df.to_csv('drug_data.csv')
