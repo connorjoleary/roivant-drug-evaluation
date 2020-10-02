38e5625cb553882a3d2914ca9ece27b184051936
connorjoleary <oleary.connor24@gmail.com>
2020-10-02 11:20:07 -0600

    Add additional data and labels

--- dash_app.py
+++ dash_app.py
@@ -9,6 +9,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 import pandas as pd
+import numpy as np
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -37,6 +38,37 @@ fig.add_trace(
 
 fig.update_layout(height=700)
 
+def direct_drug_profit(years, row, audience=20000):
+    print(years)
+    num_users = audience*(row['patents_affected_by_drug_percent']/100)*(1-row['patents_developed_inhibitors_percent']/100)
+    print(num_users)
+    return (num_users)*(row['price_annual']-row['cost_to_create_annual'])*(years-(row['time_to_dev_weeks']/52))
+
+@app.callback(
+    Output('profit_projection', "figure"),
+    [Input('datatable-interactivity', "derived_virtual_data"),
+    Input('datatable-interactivity', "derived_virtual_selected_rows"),
+    Input('datatable-interactivity', 'selected_row_ids')])
+def update_graphs(rows, derived_virtual_selected_rows, selected_row_ids):
+    if derived_virtual_selected_rows is None:
+        derived_virtual_selected_rows = []
+
+    dff = df if rows is None else pd.DataFrame(rows)
+
+    colors = ['#7FDBFF' if i in derived_virtual_selected_rows else '#0074D9'
+            for i in range(len(dff))]
+
+    if derived_virtual_selected_rows:
+        profit_df = pd.DataFrame()
+        profit_df['years'] = range(1,6)
+        print(profit_df.years)
+        print(dff.iloc[derived_virtual_selected_rows[0]])
+        profit_df['profit'] = profit_df.years.apply(direct_drug_profit, row=dff.iloc[derived_virtual_selected_rows[0]])
+
+        return px.line(profit_df, x="years", y="profit", range_y=[-1000000000, 8000000000], title='Expected Direct Profit of Drug '+dff.iloc[derived_virtual_selected_rows[0]]['name'])
+
+    return px.line()
+
 @app.callback(
     Output('datatable-row-ids-container', "figure"),
     [Input('datatable-interactivity', "derived_virtual_data"),
@@ -49,13 +81,21 @@ def update_graphs(rows, derived_virtual_selected_rows, selected_columns, selecte
 
     dff = df if rows is None else pd.DataFrame(rows)
 
-    colors = ['#7FDBFF' if i in derived_virtual_selected_rows else '#0074D9'
+    colors = [dff.iloc[derived_virtual_selected_rows[0]]['name'] if i in derived_virtual_selected_rows else 'All'
             for i in range(len(dff))]
+
+    
 
     if not selected_columns:
         selected_columns=['price_annual']
 
-    return px.box(dff, y=selected_columns[0], hover_name='name', points="all", title='Price', color=colors)
+    if selected_columns[0].endswith('percent'):
+        return px.box(dff, y=selected_columns[0], hover_name='name', points="all", title=selected_columns[0], range_y=[0, 100], color=colors)
+        
+    if type(dff[selected_columns[0]].iloc[0])==str or type(dff[selected_columns[0]].iloc[0])==np.bool_:
+        return px.bar(dff, y=selected_columns[0], hover_name='name', title=selected_columns[0], color=colors, height=700)
+
+    return px.box(dff, y=selected_columns[0], hover_name='name', points="all", title=selected_columns[0], color=colors)
 
 # fig.update_layout(
 #     plot_bgcolor=colors['background'],
@@ -65,7 +105,7 @@ def update_graphs(rows, derived_virtual_selected_rows, selected_columns, selecte
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     html.H1(
-        children='Roivant Drug Information',
+        children='Hemophillia A Drug Commercial Success Factors',
         style={
             'textAlign': 'center',
             'color': colors['text']
@@ -83,7 +123,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         sort_action="native",
         sort_mode="multi",
         column_selectable="single",
-        row_selectable="multi",
+        row_selectable="single",
         selected_columns=[],
         selected_rows=[],
         page_action="native",
@@ -91,11 +131,12 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         page_size= 10,
     ),
     dcc.Graph(id='datatable-row-ids-container'),
+    dcc.Graph(id='profit_projection'),
 
-    dcc.Graph(
-        id='example-graph-1',
-        figure=fig
-    )
+    # dcc.Graph(
+    #     id='example-graph-1',
+    #     figure=fig
+    # )
 ])
 
 if __name__ == '__main__':
--- produce_dummy_data.py
+++ produce_dummy_data.py
@@ -6,18 +6,41 @@ import string
 random.seed(1)
 np.random.seed(1)
 
-def random_number(num, mean, sts=None):
+def random_number(num, mean, sts=None, integer=True):
     if not sts:
         sts = mean*.1
-    return np.random.normal(mean, sts, num).astype(int)
+    if integer:
+        return abs(np.random.normal(mean, sts, num).astype(int))
+    return abs(np.random.normal(mean, sts, num))
 
-def random_string(choices=string.ascii_lowercase, k=13):
-    return ''.join(random.choices(string.ascii_lowercase, k=k))
+def random_string(blank='', choices=string.ascii_lowercase, k=13, return_type='string'):
+    if k=='any':
+        k=random.random()*len(choices)
 
-def produce_dummy_data(num=50):
+    if return_type=='list':
+        return random.sample(choices, int(k))
+
+    elif return_type=='bool':
+        return bool(random.getrandbits(1))
+    return ''.join(random.choices(choices, k=int(k)))
+
+def produce_dummy_data(num=25):
     df = pd.DataFrame()
-    df['price_annual'] = random_number(num, 450)
-    df['time_to_dev_weeks'] = random_number(num, 52)
+    df['price_annual'] = random_number(num, 450000)
+    df['time_to_dev_weeks'] = random_number(num, 52, 52)
+    df['cost_to_create_annual'] = random_number(num, 250000)
+    df['price_over_time'] = df.price_annual.apply(random_string, choices=['increasing', 'decreasing'], k=1, return_type='list')
+
+    df['patents_affected_by_drug_percent'] = random_number(num, 60, sts=20)
+    df['patents_developed_inhibitors_percent'] = random_number(num, 60, sts=20)
+    df['patents_died_during_trials_percent'] = random_number(num, 2, sts=3, integer=False)
+    df['side_effects'] = df.price_annual.apply(random_string, choices=['Nausea', 'Weakness', 'Headache', 'Confusion', 'Coughing up blood'], k='any', return_type='list')
+    df['works_with_inhibitors'] = df.price_annual.apply(random_string, return_type='bool')
+
+
+    # price over time
+    # Sentiment of reviews
+    # Number of people taking it over time
 
 
     df['name'] = df.price_annual.apply(random_string)
